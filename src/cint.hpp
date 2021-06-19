c19eb882bba92238b1b34c7736af91a6cf434c07
Adrien LENGLET <adrien.lenglet3@gmail.com>
2021-06-19 20:30:43 +0200

    -Fix cint assertion messages

--- src/cint.hpp
+++ src/cint.hpp
@@ -3,14 +3,14 @@
 using int8_t = char;
 using uint8_t = unsigned char;
 
-static_assert(sizeof(int8_t) == 1 && sizeof(uint8_t) == 1, "int8_t != 1");
+static_assert(sizeof(int8_t) == 1 && sizeof(uint8_t) == 1, "(u)int8_t != 1");
 
 using int16_t = short;
 using uint16_t = unsigned short;
 
-static_assert(sizeof(int16_t) == 2 && sizeof(uint16_t) == 2, "uint16_t != 2");
+static_assert(sizeof(int16_t) == 2 && sizeof(uint16_t) == 2, "(u)int16_t != 2");
 
 using int32_t = long;
 using uint32_t = unsigned long;
 
-static_assert(sizeof(int32_t) == 4 && sizeof(uint32_t) == 4, "uint32_t != 4");
+static_assert(sizeof(int32_t) == 4 && sizeof(uint32_t) == 4, "(u)int32_t != 4");
