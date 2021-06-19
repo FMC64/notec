7a2d4cc3abff3a9bebfb85e3abeb4488045195ae
Adrien LENGLET <adrien.lenglet3@gmail.com>
2021-06-19 19:35:01 +0200

    -Add test auto calling 	-> Use static variable constructor

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -2,26 +2,26 @@
 #include "test.hpp"
 #include "arith.hpp"
 
-test_list(
-	dummy_test
-);
+static size_t count = 0;
+static size_t pass = 0;
+static size_t fail = 0;
+
+void test_run(const char *name, test_t *test)
+{
+	count++;
+	try {
+		test();
+		pass++;
+	} catch (const char *e) {
+		fail++;
+		printf("NOPASS %s: %s\n", name, e);
+	}
+}
 
 int main(void)
 {
-	size_t count = array_size(tests);
-	size_t pass = 0;
-	size_t fail = 0;
-	for (size_t i = 0; i < count; i++) {
-		try {
-			tests[i]();
-			count++;
-		} catch (const char *e) {
-			fail++;
-			printf("NOPASS #%zu: %s\n", i, e);
-		}
-	}
 	printf("[SUM] PASS: %zu / %zu\n", pass, count);
 	if (fail > 0)
-		printf("/!\\ [WRN] FAIL: %zu / %zu /!\\\n", fail, count);
+		printf("\n\t/!\\   [WRN] FAIL: %zu / %zu   /!\\\n", fail, count);
 	return 0;
 }
--- src/test/test.hpp
+++ src/test/test.hpp
@@ -1,10 +1,7 @@
 #pragma once
 
-#define test_case(name) void name(void)
-
 using test_t = void (void);
 
-#define test_prototypes(first, ...) test_case(first); __VA_OPT__(test_prototypes(__VA_ARGS__))
-#define test_list_impl(first, ...) first, __VA_OPT__(test_list_impl(__VA_ARGS__))
+void test_run(const char *name, test_t *test);
 
-#define test_list(...) test_prototypes(__VA_ARGS__) static test_t *tests[] = {test_list_impl(__VA_ARGS__)}
+#define test_case(name) void name(void); static struct _caller_##name { _caller_##name() { test_run(#name, name); } } _caller_inst_##name; void name(void)
