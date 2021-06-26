730b6827622ec82cca6e469dfc156cf52e5a0a0c
Adrien LENGLET <adrien.lenglet3@gmail.com>
2021-06-26 18:53:36 +0200

    -Add Token::Op enum -Add Token::OpCplx class 	-> Table structs 	-> Encoding 	-> First node specification

--- src/Token.hpp
+++ src/Token.hpp
@@ -121,6 +121,135 @@ namespace Char {
 	static inline constexpr STable type_table = computeTypeTable();
 }
 
+enum class Op : char {
+	Not = 0,	// !
+	Plus = 1,	// +
+	Minus = 2,	// -
+	BitAnd = 3,	// &
+	BitOr = 4,	// |
+	BitXor = 5,	// ^
+	Mul = 6,	// *
+	Div = 7,	// /
+	Mod = 8,	// %
+	Less = 9,	// <
+	Greater = 10,	// >
+	Equal = 11,	// =
+	Point = 12,	// .
+
+	NotEqual = 13,	// !=
+
+	PlusPlus = 14,	// ++
+	PlusEqual = 15,	// +=
+
+	MinusMinus = 16,	// --
+	MinusEqual = 17,	// -=
+	Arrow = 18,	// ->
+
+	And = 19,	// &
+	BitAndEqual = 20,	// &=
+
+	Or = 21,	// |
+	BitOrEqual = 22,	// |=
+
+	BitXorEqual = 23,	// ^=
+
+	MulEqual = 24,	// *=
+
+	DivEqual = 25,	// /=
+	Comment = 26,	// /*
+	SLComment = 27,	// //
+
+	ModEqual = 28,	// %=
+
+	LessEqual = 29,	// <=
+	BitLeft = 30,	// <<
+	BitLeftEqual = 31,	// <<=
+
+	GreaterEqual = 32,	// >=
+	BitRight = 33,	// >>
+	BitRightEqual = 34,	// >>=
+
+	EqualEqual = 35,	// ==
+	Expand = 36,	// ...
+
+	LPar = 37,	// (
+	RPar = 38,	// )
+	LArr = 39,	// [
+	RArr = 40,	// ]
+	Tilde = 41,	// ~
+	Comma = 42,	// ,
+	Huh = 43,	// ?
+	Colon = 44,	// :
+	Semicolon = 45,	// ;
+	LBra = 46,	// {
+	RBra = 47	// }
+};
+
+namespace OpCplx {
+	struct Table
+	{
+		// 0xF0 mask next node addr
+		// 0x08 flag has next node
+		// 0x04 flag valid code
+		// 0x01 mask res ndx for output code
+		char ind[10];	// =+-><&|*/.
+		char res[2];
+
+		char pad[4];
+	};
+
+	static_assert(sizeof(Table) == 16, "Table size must be 16");
+
+	struct GTable
+	{
+		Table nodes[16];
+	};
+
+	static inline constexpr uint8_t has_next_node = 0x08;
+	static inline constexpr uint8_t has_valid_code = 0x04;
+	static inline constexpr uint8_t next_node_mask = 0xF0;
+	static inline constexpr uint8_t res_ndx_mask = 0x01;
+
+	static inline constexpr uint8_t empty = 0xFF;
+	static inline constexpr char gtb(uint8_t res_ndx, uint8_t node_ndx)
+	{
+		char res = 0;
+		if (res_ndx != empty) {
+			res |= has_valid_code;
+			res |= res_ndx & res_ndx_mask;
+		}
+		if (node_ndx != empty) {
+			res |= has_next_node;
+			res |= node_ndx & next_node_mask;
+		}
+		return res;
+	}
+	static inline constexpr GTable getTable(void)
+	{
+		return {{
+			{	// [0] !
+				{
+					gtb(0, empty),	// [0] =
+					0,	// [1] +
+					0,	// [2] -
+					0,	// [3] >
+					0,	// [4] <
+					0,	// [5] &
+					0,	// [6] |
+					0,	// [7] *
+					0,	// [8] /
+					0	// [9] .
+				},
+				{
+					static_cast<char>(Op::NotEqual)	// !=
+				},
+				{}
+			}
+		}};
+	}
+	static inline constexpr GTable table = getTable();
+}
+
 class Stream
 {
 	inline Type tok_type(char c)
