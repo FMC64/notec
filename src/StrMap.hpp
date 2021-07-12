f1273164313e943a5b203c59e79e01c3fefc4dd6
Adrien LENGLET <adrien.lenglet3@gmail.com>
2021-07-12 08:46:05 +0200

    -Add StrMap::Block struct

--- src/StrMap.hpp
+++ src/StrMap.hpp
@@ -1,3 +1,26 @@
 #pragma once
 
 #include "cint.hpp"
+
+namespace StrMap {
+
+struct Block
+{
+	char c;
+
+	/*
+	By default, next child is right after current block
+	control flag 0x01 - next child at all ?
+	control flag 0x02 - next block conttains index to next child
+	control flag 0x04 - next entry at all ?
+	control flag 0x08 - have payload, contained in two next blocks
+	All flags are compatible with each other, ordering of next blocks will be
+	0 - current block
+	1 - next child
+	2 - next entry
+	3 - payload
+	*/
+	char control;
+};
+
+}
