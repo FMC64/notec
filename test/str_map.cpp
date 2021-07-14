459a94c9e75815acbca8bf0db6e606188567a411
Adrien LENGLET <adrien.lenglet3@gmail.com>
2021-07-14 19:06:06 +0200

    -Add all C keywords insertion test

--- test/str_map.cpp
+++ test/str_map.cpp
@@ -1,4 +1,5 @@
 #include "test.hpp"
+#include "arith.hpp"
 #include "StrMap.hpp"
 
 test_case(strmap_0)
@@ -12,4 +13,48 @@ test_case(strmap_0)
 	m.insert("abe", static_cast<uint16_t>(2));
 	test_assert(m.resolve("abe", res));
 	test_assert(res == 2);
+}
+
+test_case(strmap_1)
+{
+	StrMap::BlockReserve r;
+	auto m = r.allocate();
+	const char *strs[] = {
+		"auto",
+		"break"
+		"case",
+		"char",
+		"const"
+		"continue",
+		"default",
+		"do",
+		"double",
+		"else",
+		"enum",
+		"extern",
+		"float",
+		"for",
+		"goto",
+		"if",
+		"inline",
+		"int",
+		"long",
+		"register",
+		"return",
+		"short",
+		"signed",
+		"sizeof",
+		"static",
+		"struct",
+		"switch",
+		"typedef",
+		"union",
+		"unsigned",
+		"void",
+		"volatile",
+		"while"
+	};
+
+	for (size_t i = 0; i < array_size(strs); i++)
+		m.insert(strs[i], static_cast<uint16_t>(i));
 }
