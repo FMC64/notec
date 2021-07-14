d096ae9f05c0daeae80761e41ccbc55e2e3e2030
Adrien LENGLET <adrien.lenglet3@gmail.com>
2021-07-14 18:34:22 +0200

    -Add BlockGroup::insert base implem

--- src/StrMap.hpp
+++ src/StrMap.hpp
@@ -12,7 +12,7 @@ struct Block
 	/*
 	By default, next child is right after current block
 	control flag 0x01 - next child at all ?
-	control flag 0x02 - next block contains index to next child
+	control flag 0x02 - next block is next child
 	control flag 0x04 - next entry at all ?
 	control flag 0x08 - have payload, contained in N next blocks
 	All flags are compatible with each other, ordering of next blocks will be
@@ -25,7 +25,7 @@ struct Block
 
 	struct Control {
 		static inline constexpr char has_next_child = 0x01;
-		static inline constexpr char next_child_indirect = 0x02;
+		static inline constexpr char next_child_direct = 0x02;
 		static inline constexpr char has_next_entry = 0x04;
 		static inline constexpr char has_payload = 0x08;
 	};
@@ -40,6 +40,8 @@ class BlockGroup
 	Block *m_root;
 	BlockReserve &m_reserve;
 
+	inline uint16_t end(void) const;
+
 	inline uint16_t resolve_node(const char *&str)
 	{
 		uint16_t cur_i = 0;
@@ -47,29 +49,25 @@ class BlockGroup
 			auto cur = m_root[cur_i];
 			if (cur.c == *str) {	// visit next child
 				str++;
-				if (cur.control & Block::Control::has_next_child) {
-					if (cur.control & Block::Control::next_child_indirect) {
-						cur_i = *reinterpret_cast<const uint16_t*>(m_root + cur_i + 1);
-					} else
-						cur_i++;
-				} else
+				if (cur.control & Block::Control::next_child_direct)
+					cur_i++;
+				else if (cur.control & Block::Control::has_next_child)
+					cur_i = *reinterpret_cast<const uint16_t*>(m_root + cur_i + 1);
+				else
 					return cur_i;
-			} else {	// visit next entry
 				if (*str == 0)
 					return cur_i;
-				if (cur.control & Block::Control::has_next_entry) {
-					cur_i = *reinterpret_cast<const uint16_t*>(m_root + cur_i +
-						(cur.control & Block::Control::next_child_indirect ? 1 : 0) +
-						1);
-				} else {
+			} else {	// visit next entry
+				if (cur.control & Block::Control::has_next_entry)
+					cur_i = *reinterpret_cast<const uint16_t*>(m_root + cur_i + 2);
+				else
 					return cur_i;
-				}
 			}
 		}
 	}
 
 public:
-	BlockGroup(Block *root, BlockReserve &reserve) :
+	inline BlockGroup(Block *root, BlockReserve &reserve) :
 		m_root(root),
 		m_reserve(reserve)
 	{
@@ -78,12 +76,11 @@ public:
 	template <typename T>
 	inline bool resolve(const char *str, T &res)
 	{
+		static_assert(sizeof(T) == 2, "T size must be 2");
 		auto i = resolve_node(str);
 		auto cur = m_root[i];
 		if (*str == 0 && cur.control & Block::Control::has_payload) {
-			res = *reinterpret_cast<const T*>(m_root + i +
-						(cur.control & Block::Control::next_child_indirect ? 1 : 0) +
-						(cur.control & Block::Control::has_next_entry ? 1 : 0));
+			res = *reinterpret_cast<const T*>(m_root + i + 3);
 			return true;
 		} else
 			return false;
@@ -101,10 +98,10 @@ class BlockReserve
 
 	friend class BlockGroup;
 
-	void add_blocks(size_t count)
+	inline void add_blocks(size_t count)
 	{
 		auto needed = m_count + count;
-		if (needed < m_allocated) {
+		if (needed > m_allocated) {
 			m_allocated *= 2;
 			if (m_allocated < needed)
 				m_allocated = needed;
@@ -112,33 +109,119 @@ class BlockReserve
 		}
 	}
 
+	inline void add_block(const Block &value)
+	{
+		m_buffer[m_count++] = value;
+	}
+
+	inline void add_block(uint16_t value)
+	{
+		*reinterpret_cast<uint16_t*>(&m_buffer[m_count++]) = value;
+	}
+
+	inline uint16_t end(void)
+	{
+		return m_count;
+	}
+
 public:
-	BlockReserve(void)
+	inline BlockReserve(void)
 	{
 	}
-	~BlockReserve(void)
+	inline ~BlockReserve(void)
 	{
 		std::free(m_buffer);
 	}
 
-	BlockGroup allocate(void)
+	inline BlockGroup allocate(void)
 	{
-		add_blocks(1);
+		add_blocks(2);
 		auto n = &m_buffer[m_count++];
+		m_count++;
 		n->c = 0x7F;
 		n->control = 0;
 		return BlockGroup(n, *this);
 	}
 };
 
+inline uint16_t BlockGroup::end(void) const
+{
+	return &m_reserve.m_buffer[m_reserve.end()] - m_root;
+}
+
 template <typename T>
 inline bool BlockGroup::insert(const char *str, const T &payload)
 {
-	auto i = resolve_node(str);
-	auto cur = m_root[i];
+	auto ind = resolve_node(str);
+	auto cur = m_root[ind];
 	if (*str == 0 && cur.control & Block::Control::has_payload)
 		return false;
-	// WIP
+
+	constexpr uint16_t payload_size = 1;//sizeof(T) / sizeof(uint16_t);
+	static_assert(sizeof(T) == 2, "T size must be 2");
+	auto blk_size = [this](uint16_t ndx) -> uint16_t {
+		auto cur = m_root[ndx];
+		return (cur.control & Block::Control::next_child_direct ? 1 : 3) +
+		(cur.control & Block::Control::has_payload ? payload_size : 0);
+	};
+	uint16_t entry_size;
+	uint16_t needed = *str == 0 ? 4 : 3;
+	if (cur.control & Block::Control::next_child_direct) {	// inline child
+		auto nind = ind + 1;
+		auto nsize = blk_size(nind);
+		auto next = m_root[nind];
+		while ((1 + nsize) < needed && next.control & Block::Control::next_child_direct) {	// gather as much inline children as we can and need
+			next = m_root[nind + nsize];
+			nsize += blk_size(nind + nsize);
+		}
+		entry_size = 1 + nsize;	// this is how much space we can work with from current node
+
+		auto reloc = end();
+		m_reserve.add_blocks(nsize);
+		for (size_t i = 0; i < nsize; i++)	// relocate children at end to make room on current node
+			m_reserve.add_block(m_root[nind + i]);
+		if (next.control & Block::Control::next_child_direct) {
+			m_root[end() - 1].control &= ~Block::Control::next_child_direct;
+			m_reserve.add_blocks(2);
+			m_reserve.add_block(nind + nsize);
+			m_reserve.add_block(0);
+		}
+
+		reinterpret_cast<uint16_t&>(m_root[ind + 1]) = reloc;	// set relocated child index in current node
+		m_root[ind].control &= ~Block::Control::next_child_direct;
+	} else {
+		entry_size = 3;	// we have some space but no room for any payload
+	}
+
+	// at this point we have at least 3 blocks available at ind but possibly no room for payload
+	if (entry_size < needed) {
+		auto new_c = end();
+		m_reserve.add_blocks(4);
+		for (size_t i = 0; i < 3; i++)
+			m_reserve.add_block(m_root[ind + i]);
+		m_root[ind].c = 0x7F;	// insert dummy block with current block as next entry
+		m_root[ind].control = Block::Control::has_next_entry;
+		reinterpret_cast<uint16_t&>(m_root[ind + 2]) = new_c;
+		ind = new_c;
+	}
+	// at this point we got all the space we need at ind
+	if (*str != 0) {
+		reinterpret_cast<uint16_t&>(m_root[ind + 2]) = end();	// finally add next entry
+		while (*str != 0) {
+			m_reserve.add_blocks(1);
+			Block blk{*str, Block::Control::has_next_child | Block::Control::next_child_direct};
+			m_reserve.add_block(blk);
+			str++;
+		}
+		ind = end() - 1;
+		m_root[ind].control &= ~(Block::Control::has_next_child | Block::Control::next_child_direct);
+		m_reserve.add_blocks(1);
+		m_reserve.add_block(0);
+	}
+	m_root[ind].control |= Block::Control::has_payload;
+	m_root[ind + 3] = reinterpret_cast<const Block&>(payload);
+
+	return false;
 }
 
 }
--- test/str_map.cpp
+++ test/str_map.cpp
@@ -3,4 +3,8 @@
 
 test_case(strmap_0)
 {
+	StrMap::BlockReserve r;
+	auto m = r.allocate();
+	m.insert("abc", static_cast<uint16_t>(1));
+	m.insert("abe", static_cast<uint16_t>(2));
 }
