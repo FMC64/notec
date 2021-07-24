9eab2765e827efebdf624f9e2f72204cd6e30b56
Adrien LENGLET <adrien.lenglet3@gmail.com>
2021-07-24 21:39:36 +0200

    -Add argument parsing inside of __VA_OPT__ 	-> Recursive implementation

--- src/Pp.cpp
+++ src/Pp.cpp
@@ -20,6 +20,69 @@ bool Pp::next_token_dir_include(char* &res)
 	return _next_token_dir<true>(res);
 }
 
+void Pp::define_add_token(char *n, bool has_va, const char *args, const char *arg_top)
+{
+	auto size = Token::whole_size(n);
+	if (Token::type(n) == Token::Type::Identifier) {
+		if (streq(n + 1, va_args)) {
+			if (!has_va)
+				m_stream.error("Non-variadic macro");
+			alloc(2);
+			m_buffer[m_size++] = TokType::arg;
+			m_buffer[m_size++] = 0x80;
+			return;
+		}
+		if (streq(n + 1, va_opt)) {
+			if (!has_va)
+				m_stream.error("Non-variadic macro");
+			alloc(2);
+			m_buffer[m_size++] = TokType::opt;
+			auto &count = reinterpret_cast<uint8_t&>(m_buffer[m_size++]);
+			if (!next_token_dir(n))
+				m_stream.error("Expected token");
+			if (!Token::is_op(n, Token::Op::LPar))
+				m_stream.error("Expected '('");
+			uint8_t c = 0;
+			while (true) {
+				if (!next_token_dir(n))
+					m_stream.error("Expected token");
+				if (Token::is_op(n, Token::Op::RPar))
+					break;
+				c++;
+				define_add_token(n, has_va, args, arg_top);
+			}
+			count = c;
+			return;
+		}
+		auto data = Token::data(n);
+		auto cur = args;
+		uint8_t ndx = 0;
+		while (cur < arg_top) {
+			bool match = true;
+			for (uint8_t i = 0; i < size; i++) {
+				if (data[i] != *cur) {
+					match = false;
+					break;
+				}
+				cur++;
+			}
+			if (match && *cur == 0) {
+				alloc(2);
+				m_buffer[m_size++] = TokType::arg;
+				m_buffer[m_size++] = ndx;
+				return;
+			}
+			while (*cur != 0)
+				cur++;
+			cur++;
+			ndx++;
+		}
+	}
+	alloc(size);
+	for (size_t i = 0; i < size; i++)
+		m_buffer[m_size++] = n[i];
+}
+
 char* Pp::next(void)
 {
 	while (true) {
--- src/Pp.hpp
+++ src/Pp.hpp
@@ -122,6 +122,8 @@ private:
 	static inline constexpr const char* va_args = "\xB__VA_ARGS__";
 	static inline constexpr const char* va_opt = "\xA__VA_OPT__";
 
+	void define_add_token(char *n, bool has_va, const char *args, const char *arg_top);
+
 	inline char* define(void)
 	{
 		char *n;
@@ -180,71 +182,8 @@ private:
 					arg_count = 1;
 			}
 			m_buffer[m_size++] = arg_count | (has_va ? 0x80 : 0);
-			while (next_token_dir(n)) {
-				auto size = Token::whole_size(n);
-				if (Token::type(n) == Token::Type::Identifier) {
-					if (streq(n + 1, va_args)) {
-						if (!has_va)
-							m_stream.error("Non-variadic macro");
-						alloc(2);
-						m_buffer[m_size++] = TokType::arg;
-						m_buffer[m_size++] = 0x80;
-						continue;
-					}
-					if (streq(n + 1, va_opt)) {
-						if (!has_va)
-							m_stream.error("Non-variadic macro");
-						alloc(2);
-						m_buffer[m_size++] = TokType::opt;
-						auto &count = reinterpret_cast<uint8_t&>(m_buffer[m_size++]);
-						if (!next_token_dir(n))
-							m_stream.error("Expected token");
-						if (!Token::is_op(n, Token::Op::LPar))
-							m_stream.error("Expected '('");
-						uint8_t c = 0;
-						while (true) {
-							if (!next_token_dir(n))
-								m_stream.error("Expected token");
-							if (Token::is_op(n, Token::Op::RPar))
-								break;
-							c++;
-							auto size = Token::whole_size(n);
-							alloc(size);
-							for (size_t i = 0; i < size; i++)
-								m_buffer[m_size++] = n[i];
-						}
-						count = c;
-						continue;
-					}
-					auto data = Token::data(n);
-					auto cur = args;
-					uint8_t ndx = 0;
-					while (cur < arg_top) {
-						bool match = true;
-						for (uint8_t i = 0; i < size; i++) {
-							if (data[i] != *cur) {
-								match = false;
-								break;
-							}
-							cur++;
-						}
-						if (match && *cur == 0) {
-							alloc(2);
-							m_buffer[m_size++] = TokType::arg;
-							m_buffer[m_size++] = ndx;
-							goto dir_token_processed;
-						}
-						while (*cur != 0)
-							cur++;
-						cur++;
-						ndx++;
-					}
-				}
-				alloc(size);
-				for (size_t i = 0; i < size; i++)
-					m_buffer[m_size++] = n[i];
-				dir_token_processed: continue;
-			}
+			while (next_token_dir(n))
+				define_add_token(n, has_va, args, arg_top);
 		} else {	// zero token macro
 			m_buffer[m_size++] = 0;	// zero args
 			n = next_token();
