68540258e757ddae824cb114dd4a2603485d64ea
Adrien LENGLET <adrien.lenglet3@gmail.com>
2021-07-24 11:53:24 +0200

    -Fix two directives next to each other 	-> Assume directive next token can return another #

--- src/Pp.cpp
+++ src/Pp.cpp
@@ -4,11 +4,13 @@ char* Pp::next(void)
 {
 	while (true) {
 		auto n = next_token();
-		if (n == nullptr)
-			return nullptr;
-		if (Token::is_op(n, Token::Op::Sharp))
-			return directive();
-		else
-			return n;
+		while (true) {
+			if (n == nullptr)
+				return nullptr;
+			if (Token::is_op(n, Token::Op::Sharp))
+				n = directive();
+			else
+				return n;
+		}
 	}
 }
