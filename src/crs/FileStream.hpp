f04032eccfe3e0a0d4d46fbcd6df53a67ae849cb
Adrien LENGLET <adrien.lenglet3@gmail.com>
2021-08-04 14:47:14 +0200

    -Add FileStream::read, seek and close implems

--- src/crs/FileStream.hpp
+++ src/crs/FileStream.hpp
@@ -1,26 +1,48 @@
 #pragma once
 
 #include "cint.hpp"
+#include <fxlib.h>
+
+static void tactical_exit(const char *msg)
+{
+	locate(1, 1);
+	Print(reinterpret_cast<const uint8_t*>(msg));
+	while (1) {
+		unsigned int key;
+		GetKey(&key);
+	}
+}
 
 class Stream
 {
+	int m_handle;
+
 public:
 	// returns 0 on EOF
-	size_t read(char *buf, size_t size)
+	inline size_t read(char *buf, size_t size)
 	{
-		return 0;
+		auto s = Bfile_ReadFile(m_handle, buf, size, -1);
+		if (s < 0)
+			tactical_exit("Bfile_ReadFile failed");
+		return s;
 	}
 
-	bool open(const char *filepath, char *&stack, const char *stack_top)
+	inline bool open(const char *filepath, char *&stack, const char *stack_top)
 	{
 		return true;
 	}
 
-	void seek(size_t ndx)
+	inline void seek(size_t ndx)
 	{
+		auto r = Bfile_SeekFile(m_handle, ndx);
+		if (r < 0)
+			tactical_exit("Bfile_SeekFile failed");
 	}
 
-	void close(void)
+	inline void close(void)
 	{
+		auto r = Bfile_CloseFile(m_handle);
+		if (r < 0)
+			tactical_exit("Bfile_CloseFile failed");
 	}
 };
